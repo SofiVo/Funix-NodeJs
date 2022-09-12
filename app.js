043a9852ffc4221140ae30b53258ce47ce8866b4
SofiVo <112798238+SofiVo@users.noreply.github.com>
2022-09-12 20:15:04 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Lab 1.1-1.6
+
+const http = require("http");
+const routes = require("./routes.js");
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
